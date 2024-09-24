063fd1fab06a655fadadfd459fd408b29c3e124e
pawanshettyy <pawanshetty13092006@gmail.com>
2024-09-24 20:45:19 +0530

    till now

---
+++ sem-1/ELEC-WS/WS-1_arduino.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (4, HIGH);
+  delay(10000);
+  digitalWrite (4, LOW);
+  delay(10000);
+}
+
+
+//open this in arduino ide only
