65604c3625c5cfa615ab35712b5bbc9cc067760d
pawanshettyy <pawanshetty13092006@gmail.com>
2024-09-27 14:23:58 +0530

    ws 2

---
+++ sem-1/ELEC-WS/WS-2_arduino.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  delay(1000);
+
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(1000);
+
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  delay(1000);
+}
