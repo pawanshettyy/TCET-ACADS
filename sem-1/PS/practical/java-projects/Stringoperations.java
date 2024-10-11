a10922e6e0ab7251927b3c21cd7551e8b0572795
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-11 17:42:39 +0530

    skibdi

--- sem-1/PS/practical/java-projects/Stringoperations.java
+++ sem-1/PS/practical/java-projects/Stringoperations.java
@@ -7,10 +7,23 @@ public class Stringoperations{
     System.out.println("Enter a string");
     String originalstr = input.nextLine();
 
+    //copy string
     String copiedstr = originalstr;
     System.out.println("Copied String= "+copiedstr);
 
+    //string length
     int strlength = copiedstr.length();
     System.out.println("Length of the string: "+strlength);
+
+    //concat string
+    String  newstr = input.nextLine();
+    String Concatenatedstr = newstr.concat(copiedstr);
+    System.out.println("Concatenated string: " +Concatenatedstr);
+
+    //trim function
+    String trimstr = copiedstr.trim();
+    System.out.println("Trim Function: "+trimstr);
+
+    input.close();
     } 
 }
