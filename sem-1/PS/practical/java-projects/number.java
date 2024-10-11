c633e63b202c849882ef8ff1dde22422f80be148
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-11 18:01:07 +0530

    sui

--- sem-1/PS/practical/java-projects/number.java
+++ sem-1/PS/practical/java-projects/number.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class NumberCheck {
+public class number {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
