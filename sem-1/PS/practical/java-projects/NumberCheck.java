9dfbc803e9ca34d07d366544b94d19e53be071bf
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-11 17:58:53 +0530

    commit

---
+++ sem-1/PS/practical/java-projects/NumberCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        
+        if (number > 50) {
+            System.out.println("The number is greater than 50.");
+        } else if (number == 50) {
+            System.out.println("The number is equal to 50.");
+        } else {
+            System.out.println("The number is less than 50.");
+        }
+
+        
+        scanner.close();
+    }
+}
