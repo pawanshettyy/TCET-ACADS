4e0b752be8fb152174e74334f3cbb5afccef8c22
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-15 11:17:25 +0530

    pratical

---
+++ sem-1/PPS/Practical-lab/ppsExp3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int i,n;
+    float x;
+
+    printf("Enter the value of x and the value of n: ");
+    scanf("%f %d", &x , &n);
+
+    for (i=2; i<=n; i++){
+        printf("%f raised to %d is %f\n", x, i, pow(x,i));
+    }
+}
