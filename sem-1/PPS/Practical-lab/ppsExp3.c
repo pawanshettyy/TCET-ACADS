efe094e990c3795f98928461985bb4e10eaa836a
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-15 11:21:45 +0530

    practical changes

--- sem-1/PPS/Practical-lab/ppsExp3.c
+++ sem-1/PPS/Practical-lab/ppsExp3.c
@@ -1,3 +1,15 @@
+/*Algorithm
+1. Start
+2. Declare variables: i, n, x 
+3. Input the values of x and n from the user.
+4. loop for i from 2 to n
+5. result = pow(x,i)
+6. x raised to i is result
+7. End
+*/
+
+
+
 #include<stdio.h>
 #include<math.h>
 
